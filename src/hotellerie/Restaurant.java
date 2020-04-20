b504150580a72bbae7d0d3a0b9ac5f7f34295ca4
Kassebi Farouk <faroukkassebi@gmail.com>
2020-04-20 21:13:30 +0100

    class Restaurant

--- src/hotellerie/Restaurant.java
+++ src/hotellerie/Restaurant.java
@@ -2,8 +2,7 @@ package hotellerie;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Restaurant {
 
@@ -20,40 +19,69 @@ public class Restaurant {
     }
     private String Nom_R;
     private  int Nb_Fourchette;
+    private int Nb_Plat;
     private Plat [] Plats;
+    public int Calcul_Nb_Plat(){
+        String pathFichier="src\\Hotellerie\\Files\\"+getNom_R()+".txt";
+        int i=-1;
+		BufferedReader fluxEntree=null;
+		try {
+			/* Création du flux vers le fichier texte */
+			fluxEntree = new BufferedReader(new FileReader(pathFichier));
+			
+			/* Lecture d'une ligne */
+			String ligneLue = fluxEntree.readLine();
+			while(ligneLue!=null){
+                                
+				ligneLue = fluxEntree.readLine();                                                      
+                                i++;
+                                 
+			}
+                        
+                        fluxEntree.close();
+                        
+		}
+		catch(IOException exc){
+			exc.printStackTrace();
+		}
+                return i;
+    }
     public Plat [] importer_plat() {
-        Plat [] p= new Plat[11];
-        String pathFichier="src\\Hotellerie\\Files\\"+getNom_R();
+        Plat [] p= new Plat[this.getNb_Plat()];
+        String pathFichier="src\\Hotellerie\\Files\\"+getNom_R()+".txt";
 
 		BufferedReader fluxEntree=null;
 		try {
 			fluxEntree = new BufferedReader(new FileReader(pathFichier));
 			
 			String ligneLue = fluxEntree.readLine();
-			int taille=0;
-                        while(ligneLue!=null){
-				String[] tab=ligneLue.split("-");
-                                p[taille].code=tab[0];
-                                p[taille].nom=tab[1];
-                                p[taille].prix=Integer.parseInt(tab[2]);                           
-				ligneLue = fluxEntree.readLine();
-                                taille++;
-			}                                                
+			
+                        
+                        int i = 0;
+                        String [] tab1= new String [this.getNb_Plat()+1];
+			while(ligneLue!=null){
+                                tab1[i]=ligneLue;
+				ligneLue = fluxEntree.readLine();                                                      
+                                i++;
+                                 
+			}
+                         fluxEntree.close();
+                        for(int j=0;j<this.getNb_Plat();j++){
+                                
+				String[] tab=tab1[j].split("-");
+                                p[j]=new Plat(tab[0], tab[1], Integer.parseInt(tab[2]));
+                                
+                                
+                                
+                                
+			} 
+                        
 		}
 		catch(IOException exc){
 			exc.printStackTrace();
 		}
-		finally{
-			try{
-				if(fluxEntree!=null){
-					/* Fermeture du flux vers le fichier */
-					fluxEntree.close();
-				}
-			}
-			catch(IOException e){
-				e.printStackTrace();
-			}
-		}	
+		
+		
     return p;
     }
     
@@ -61,52 +89,36 @@ public class Restaurant {
     public Restaurant (String n, int nb) {
         Nom_R=n;
         Nb_Fourchette=nb;
-        Plat [] p= new Plat[11];
-        p=this.importer_plat();
-        
+        Nb_Plat=Calcul_Nb_Plat();
+        Plats=this.importer_plat();
     }
-
-    /**
-     * @return the Nom_R
-     */
     public String getNom_R() {
         return Nom_R;
     }
-
-    /**
-     * @param Nom_R the Nom_R to set
-     */
     public void setNom_R(String Nom_R) {
         this.Nom_R = Nom_R;
     }
 
-    /**
-     * @return the Nb_Fourchette
-     */
-    public int getNb_Fourchette() {
+       public int getNb_Fourchette() {
         return Nb_Fourchette;
     }
+       public int getNb_Plat() {
+           return Nb_Plat;
+       }
 
-    /**
-     * @param Nb_Fourchette the Nb_Fourchette to set
-     */
-    public void setNb_Fourchette(int Nb_Fourchette) {
+        public void setNb_Fourchette(int Nb_Fourchette) {
         this.Nb_Fourchette = Nb_Fourchette;
     }
 
-    /**
-     * @return the Plats
-     */
-    public Plat[] getPlats() {
+        public Plat[] getPlats() {
         return Plats;
     }
 
-    /**
-     * @param Plats the Plats to set
-     */
-    public void setPlats(Plat[] Plats) {
+        public void setPlats(Plat[] Plats) {
         this.Plats = Plats;
     }
     
+        
+          
     
 }
